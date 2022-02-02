a0de6b5a845c0900c4b05f19ea35e244baf1ef22
Matheus <matheusfoaraujo@gmail.com>
2022-02-02 17:47:33 -0300

    adiciona novos comandos ao arquivo arithmetic

--- fundamentos/Bloco-4.1/arithmetic-operators.js
+++ fundamentos/Bloco-4.1/arithmetic-operators.js
@@ -1,10 +1,13 @@
-let num1 = 5;
-let num2 = 10;
-let num3 = 20;
+//let num1 = 5;
+//let num2 = 10;
+//let num3 = 20;
 
-console.log(num1);
-console.log(num2);
-console.log(num3);
+//.log(num1);
+//console.log(num2);
+//console.log(num3);
 
-let mybox=null;
-console.log(mybox);
+//let mybox=null;
+//console.log(mybox);
+
+let isEven = 5%2 == 0;
+console.log(isEven);
